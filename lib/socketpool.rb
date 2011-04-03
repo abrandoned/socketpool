3a6533ff36ae16e98827fb79a3a387c21b3231cd
Brandon Dewitt <brandon@myjibe.com>
2011-04-03 00:03:23 -0400

    initial commit

---
+++ lib/socketpool.rb
@@ -0,0 +1,174 @@
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require 'socket'
+require 'thread'
+
+class SocketPool
+
+  attr_accessor :host, :port, :size, :timeout, :checked_out
+
+  # Create a new socket pool
+  #
+  def initialize(host, port, opts={})
+    @host, @port = host, port
+
+    # Pool size and timeout.
+    @size      = opts[:size] || 2
+    @timeout   = opts[:timeout]   || 5.0
+    @eager      = opts[:eager] || false
+
+    # Mutex for synchronizing pool access
+    @connection_mutex = Mutex.new
+
+    # Condition variable for signal and wait
+    @queue = ConditionVariable.new
+    
+    @socktype     = opts[:type] || :tcp
+    @sockopts     = opts[:socketopts].nil? ? [] : [opts[:socketopts]].flatten.inject([]){|s, so| s << so}
+    @sockets      = []
+    @pids         = {}
+    @checked_out  = []
+    
+    initialize_socketpool if @eager
+  end
+  
+  def close
+    @sockets.each do |sock|
+      begin
+        sock.close
+      rescue IOError => ex
+        warn "IOError when attempting to close socket connected to #{@host}:#{@port}: #{ex.inspect}"
+      end
+    end
+    @host = @port = nil
+    @sockets.clear
+    @pids.clear
+    @checked_out.clear
+  end
+  
+  # Return a socket to the pool.
+  def checkin(socket)
+    @connection_mutex.synchronize do
+      @checked_out.delete(socket)
+      @queue.signal
+    end
+    true
+  end
+
+  # Adds a new socket to the pool and checks it out.
+  #
+  # This method is called exclusively from #checkout;
+  # therefore, it runs within a mutex.
+  def checkout_new_socket
+    begin
+    socket = Socket.new(so_domain(@socktype), so_type(@socktype), 0)
+    @sockaddr ||= Socket.pack_sockaddr_in(@port, @host) if ![:unix, :unigram].include?(@socktype)
+    @sockaddr ||= Socket.pack_sockaddr_un(@host) if [:unix, :unigram].include?(@socktype)
+    socket.connect(@sockaddr)
+    if @sockopts.size > 0
+      @sockopts.each{ |opt| socket.setsockopt(opt[:level], opt[:optname], opt[:optval]) }  
+    end
+    rescue => ex
+      raise ConnectionFailure, "Failed to connect to host #{@host} and port #{@port}: #{ex}"
+    end
+
+    @sockets << socket
+    @pids[socket] = Process.pid
+    @checked_out << socket
+    socket
+  end
+
+  # Checks out the first available socket from the pool.
+  #
+  # If the pid has changed, remove the socket and check out
+  # new one.
+  #
+  # This method is called exclusively from #checkout;
+  # therefore, it runs within a mutex.
+  def checkout_existing_socket
+    socket = (@sockets - @checked_out).first
+    if @pids[socket] != Process.pid
+       @pids[socket] = nil
+       @sockets.delete(socket)
+       socket.close
+       checkout_new_socket
+    else
+      @checked_out << socket
+      socket
+    end
+  end
+
+  # Check out an existing socket or create a new socket if the maximum
+  # pool size has not been exceeded. Otherwise, wait for the next
+  # available socket.
+  def checkout
+    start_time = Time.now
+    loop do
+      if (Time.now - start_time) > @timeout
+          raise ConnectionTimeoutError, "could not obtain connection within " +
+            "#{@timeout} seconds. The max pool size is currently #{@size}; " +
+            "consider increasing the pool size or timeout."
+      end
+
+      @connection_mutex.synchronize do
+        socket = if @checked_out.size < @sockets.size
+                   checkout_existing_socket
+                 elsif @sockets.size < @size
+                   checkout_new_socket
+                 end
+
+        if socket
+          return socket
+        else
+          # Otherwise, wait
+          @queue.wait(@connection_mutex)
+        end
+      end
+    end
+  end
+  
+  def so_type(val)
+    val = val.downcase.to_sym if val.respond_to?(:downcase) && val.respond_to?(:to_sym)
+    @so_type ||= {
+      :tcp => Socket::SOCK_STREAM,
+      :tcp6 => Socket::SOCK_STREAM,
+      :udp => Socket::SOCK_DGRAM,
+      :udp6 => Socket::SOCK_DGRAM,
+      :unix => Socket::SOCK_STREAM,
+      :unigram => Socket::SOCK_DGRAM
+    }[val]
+  end
+  
+  def so_domain(val)
+    val = val.downcase.to_sym if val.respond_to?(:downcase) && val.respond_to?(:to_sym)
+    @so_domain ||= {
+      :tcp => Socket::AF_INET,
+      :tcp6 => Socket::AF_INET6,
+      :udp => Socket::AF_INET,
+      :udp6 => Socket::AF_INET6,
+      :unix => Socket::AF_UNIX,
+      :unigram => Socket::AF_UNIX
+    }[val]
+  end
+
+  private 
+  
+  def initialize_socketpool
+    begin
+      @size.times{ checkout_new_socket }
+    ensure
+      @checked_out = []
+    end
+  end
+end
--- spec/socketpool_spec.rb
+++ spec/socketpool_spec.rb
@@ -1,7 +1,75 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "Socketpool" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+describe "SocketPool arguments" do
+  describe SocketPool.new('127.0.0.1', '11222') do 
+    specify { subject.port.should eq('11222')}
+    specify { subject.host.should eq('127.0.0.1')}
+    specify { subject.size.should eq(2)}
+    specify { subject.timeout.should eq(5.0)}
+    specify { subject.instance_variable_get(:@sockets).should eq([])}
+    specify { subject.instance_variable_get(:@checked_out).should eq([])}
+    specify { subject.instance_variable_get(:@pids).should eq({})}
+    specify { subject.instance_variable_get(:@eager).should eq(false)}
+    specify { subject.instance_variable_get(:@socktype).should eq(:tcp)}
+    specify { subject.instance_variable_get(:@sockopts).should eq([])}
+    
+    it "should create socket on checkout" do 
+      s = subject.checkout
+      subject.instance_variable_get(:@sockets).should_not eq([])
+      subject.instance_variable_get(:@sockets).size.should eq(1)
+      subject.instance_variable_get(:@checked_out).size.should eq(1)
+
+      subject.checkin(s)
+      subject.instance_variable_get(:@checked_out).size.should eq(0)
+    end    
+  end
+
+  describe "UDP => Eager SocketPool" do 
+    subject { SocketPool.new('127.0.0.1', '11222', :type => :udp, :eager => true) }
+    
+    specify { subject.port.should eq('11222')}
+    specify { subject.host.should eq('127.0.0.1')}
+    specify { subject.size.should eq(2)}
+    specify { subject.timeout.should eq(5.0)}
+    specify { subject.instance_variable_get(:@sockets).size.should eq(2)}
+    specify { subject.instance_variable_get(:@checked_out).should eq([])}
+    specify { subject.instance_variable_get(:@eager).should eq(true)}
+    specify { subject.instance_variable_get(:@socktype).should eq(:udp)}
+    specify { subject.instance_variable_get(:@sockopts).should eq([])}    
+  end
+
+  describe "UDP => Eager SocketPool => resized, short timeout" do 
+    subject { SocketPool.new('127.0.0.1', '11222', :type => :udp, :eager => true, :size => 19, :timeout => 1) }
+    
+    specify { subject.port.should eq('11222')}
+    specify { subject.host.should eq('127.0.0.1')}
+    specify { subject.size.should eq(19)}
+    specify { subject.timeout.should eq(1.0)}
+    specify { subject.instance_variable_get(:@sockets).size.should eq(19)}
+    specify { subject.instance_variable_get(:@checked_out).should eq([])}
+    specify { subject.instance_variable_get(:@eager).should eq(true)}
+    specify { subject.instance_variable_get(:@socktype).should eq(:udp)}
+    specify { subject.instance_variable_get(:@sockopts).should eq([])}    
+  end
+
+  describe "TCP => Eager SocketPool => resized, short timeout => TCP_NODELAY" do 
+    subject { 
+      SocketPool.new('127.0.0.1', '11222', 
+        :type => :tcp, 
+        :eager => true, 
+        :size => 19, 
+        :timeout => 1, 
+        :socketopts => [{:level => Socket::IPPROTO_TCP, :optname => Socket::TCP_NODELAY, :optval => 1}]) 
+    }
+    
+    specify { subject.port.should eq('11222')}
+    specify { subject.host.should eq('127.0.0.1')}
+    specify { subject.size.should eq(19)}
+    specify { subject.timeout.should eq(1.0)}
+    specify { subject.instance_variable_get(:@sockets).size.should eq(19)}
+    specify { subject.instance_variable_get(:@checked_out).should eq([])}
+    specify { subject.instance_variable_get(:@eager).should eq(true)}
+    specify { subject.instance_variable_get(:@socktype).should eq(:tcp)}
+    specify { subject.instance_variable_get(:@sockopts).should_not eq([])}    
   end
 end
